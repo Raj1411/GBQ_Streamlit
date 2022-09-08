26bfdf6a0d8abeed320a2bb5ac5bce228ce868d7
Raj1411 <56500451+Raj1411@users.noreply.github.com>
2022-09-08 23:54:58 +0530

    Update main.py

--- main.py
+++ main.py
@@ -5,8 +5,7 @@ from google.oauth2 import service_account
 from google.cloud import bigquery
 
 
-credentials = service_account.Credentials.from_service_account_info(
-    st.secrets["gcp_service_account"]
+credentials = service_account.Credentials.from_service_account_info(st.secrets["gcp_service_account"])
 client = bigquery.Client(credentials=credentials)
 
     
