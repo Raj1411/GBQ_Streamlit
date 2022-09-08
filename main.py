9454068d1714e9a60b719a49601e5d12a86f97f1
Raj1411 <56500451+Raj1411@users.noreply.github.com>
2022-09-08 23:04:15 +0530

    Update main.py

--- main.py
+++ main.py
@@ -9,10 +9,10 @@ def main():
     if file_upload:
          if file_upload.type == "text/csv":
             df = pd.read_csv(file_upload)
-            df.to_gbq(destination_table="Daily_sales_report.Sep-22", project_id=" crafty-hook-331815", if_exists="append")
+            df.to_gbq(destination_table="Daily_sales_report.Big_Basket", project_id="crafty-hook-331815", if_exists="append")
          else:
             df = pd.read_excel(file_upload)
-            df.to_gbq(destination_table="Daily_sales_report.Sep-22", project_id=" crafty-hook-331815", if_exists="append")
+            df.to_gbq(destination_table="Daily_sales_report.Big_Basket", project_id="crafty-hook-331815", if_exists="append")
 
 
 # df = pd.read_csv("D:\Office\Flipkart & FBF\Python\Sales data to Big Query\manufacturer_sales-report-gamma_01-09-2022_08-09-2022_1662614157.39.csv")
